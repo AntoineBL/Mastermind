cedbc55e30f2011cfbf72383a7ea16af0415c00d
Raph <raphael.fustes@gmail.com>
2018-04-27 20:20:38 -0400

    Fonction pour creer le setS + début algo minimax

--- src/jeu/Jeu.java
+++ src/jeu/Jeu.java
@@ -2,30 +2,38 @@ package jeu;
 
 import joueur.ia.IAJoueurPassif;
 import joueur.ia.IA_aleatoire;
+import joueur.ia.IA_minimax;
 import mastermind.CodeSolution;
 import mastermind.Plateau;
 
 public class Jeu {
 
 	private IAJoueurPassif passif;
-	private IA_aleatoire actif;
+	private IA_minimax actif;
 	private Plateau p;
-	private int nbTrou = 4;
-	private int nbCouleur = 6;
+	private int nbTrou = 2;
+	private int nbCouleur = 3;
 	
 	public Jeu() {
 		p = new Plateau(nbTrou, nbCouleur);
 		passif= new IAJoueurPassif(p);
-		actif = new IA_aleatoire(p);
+		actif = new IA_minimax(p);
 	}
 	
 	
 	public void jouer() {
 		
+		
 		passif.choisirCode(new CodeSolution(nbTrou, nbCouleur));
 		
 		int nbTour = 0;
 		 do{
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			nbTour++;
 			System.out.println("--------");
 			System.out.println("Tour "+nbTour);
--- src/joueur/ia/IA_minimax.java
+++ src/joueur/ia/IA_minimax.java
@@ -1,18 +1,109 @@
 package joueur.ia;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+import mastermind.LigneProposition;
 import mastermind.Plateau;
 
 public class IA_minimax extends IA{
 
+	private List<LigneProposition> setS = new LinkedList<LigneProposition>();
+	private List<LigneProposition> setU;
+	private boolean premierTour;
+	LigneProposition l = null;
 	
 	public IA_minimax(Plateau p) {
 		super(p);
-		// TODO Auto-generated constructor stub
+		premierTour = true;
+		
+//		setS.add(new LigneProposition(1,1));
+//		setS.add(new LigneProposition(1,2));
+//		setS.add(new LigneProposition(1,3));
+//		setS.add(new LigneProposition(2,1));
+//		setS.add(new LigneProposition(2,2));
+//		setS.add(new LigneProposition(2,3));
+//		setS.add(new LigneProposition(3,1));
+//		setS.add(new LigneProposition(3,2));
+//		setS.add(new LigneProposition(3,3));
+		
+		setS = createSet();
+		
+		setU = new LinkedList<LigneProposition>(setS);
+
+
+	}
+	
+	public LinkedList<LigneProposition> createSet() {
+		LinkedList<LigneProposition> l =  new LinkedList<>();
+		LigneProposition lp, lplast;
+		
+		int nbSeta = (int) Math.pow(p.getNbCouleur(), p.getNbTrou());
+		
+		lp = new LigneProposition(p.getNbTrou());
+		for(int j =0; j<p.getNbTrou(); j++) {
+			lp.setAt(j, 1);
+		}
+		l.add(lp);
+
+		for(int i = 1; i<nbSeta; i++) {
+			lplast = lp;
+			lp = new LigneProposition(p.getNbTrou());
+			for(int j =0; j<p.getNbTrou(); j++) {
+				lp.setAt(j, lplast.getAt(j));
+			}
+			
+			lp.setAt(p.getNbTrou()-1, lplast.getAt(p.getNbTrou()-1)+1);
+			for(int j = p.getNbTrou()-1; j>=0; j--) {
+				if(lp.getAt(j) == p.getNbCouleur()+1) {
+					lp.setAt(j, 1);
+					if(j >= 0) {
+						lp.setAt(j-1, lp.getAt(j-1)+1);
+					}
+				}
+			}
+			
+			l.add(lp);
+		}
+			
+		return l;
 	}
 	
 	
-	//choisir un code aleatoirement
 	public void resolution() {
+		
+		int index = (int)(Math.random() * setS.size());
+		System.out.println("Size: " + setS.size());
+		System.out.println("Index " + index);
+
+		
+		if(premierTour) {
+			l = setS.get(index);
+			p.ajouterEssai(l);
+			premierTour = false;
+		} 
+		else {
+			
+//			index = (int)(Math.random() * setS.size());
+//			System.out.println("Size: " + setS.size());
+//			System.out.println("Index " + index);
+//			l = setS.get(index);
+//			p.ajouterEssai(l);
+//			setS.remove(l);
+			l = setS.get(index);
+			System.out.println("l : "+ l);
+			setS.remove(l);
+			System.out.println("FFFFFFFF");
+			for (LigneProposition ligneProposition : setU) {
+				System.out.println(ligneProposition);
+			}
+		}
+	
+
+		
 		
 	}
 
